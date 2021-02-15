582141c52cccf037a5584a6e3eab1d229dd9519e
Sorokina Valeria <unicornfish@yande.ru>
2021-02-16 00:08:45 +0300

    первая функция

--- parser.py
+++ parser.py
@@ -1,3 +1,5 @@
+import re
+
 try:
     with open('test_schedule.inc',"r",encoding = 'utf-8') as file:
         test_file = file.read()
@@ -5,3 +7,10 @@ except FileNotFoundError:
     print('Файл не найден')
     
 print(test_file)
+print("=======================")
+
+def clean_schedule(file):
+    del_comment = list(map((lambda x: re.sub(r'--[\w\s,()]+','',x)),file.split('\n')))
+    return '\n'.join(list(filter(lambda x: x, del_comment)))
+
+wihout_comment = clean_schedule(test_file)
